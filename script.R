f9c90e23041ea600028d5a8d54c50253bab796fd
Haziq Hapiz <zyqq.dev@gmail.com>
2021-11-20 23:00:24 +0800

    init

---
+++ script.R
@@ -0,0 +1,14 @@
+# link(dataset/covid19-public-main/vaccination/vax_malaysia.csv, "vax_malaysia.csv")
+
+install.packages("languageserver")
+
+print(paste0("vax_malaysia.csv", "vax_malaysia.csv"))
+
+print("hello world")
+
+vax_malaysia <- read.csv("E:/Programming/R/stats-assignment/dataset/covid19-public-main/vaccination/vax_malaysia.csv")
+
+
+# list(dataset/covid19-public-main/vaccination/vax_malaysia.csv, "vax_malaysia.csv")
+
+list(vax_malaysia)
